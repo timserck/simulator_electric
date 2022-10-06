5b62481ef26d64c12f6b4dd587b05859f1baf39f
Timothée SERCK <timotheeserck@MP041.local>
2022-10-06 18:41:40 +0200

    adapt result data

--- src/Components/result.component.js
+++ src/Components/result.component.js
@@ -9,28 +9,38 @@ export const Result = (props) => {
 
   const context = useContext(modelsContext)
   const {currentValues, models, currentModel} = context;
+
   let slidesValues =  {
-    km  : currentValues[0],
-    conso  : currentValues[1],
-    carbu  : currentValues[2],
-    elec  : currentValues[3],
+    km  : Number(currentValues[0]),
+    conso  : Number(currentValues[1]),
+    carbu  : Number(currentValues[2]),
+    elec  : Number(currentValues[3]),
   }
+
+
 
   const getCurrentModel = () => {
     return models.filter(model => model.name === currentModel)[0]
    }
 
+  //  ( [slider kilométrage] * [slider conso carburant] / 100 * [slider coût carburant] )
+  //  - 
+  //  ( [slider kilométrage] * [ligne 8 du Excel pour ce véhicule] / 100 * [slider coût élec] )
+
   return (
     <div className='result'>
       <span className="result-value">
-      {txt === "Sur 1 mois" && (slidesValues.km * slidesValues.conso / 100  * slidesValues.conso ) - ( slidesValues.km * getCurrentModel().WLTP_consumption / 100 * slidesValues.km) < 0 ? 0 : position === 0 && (slidesValues.km * slidesValues.conso / 100  * slidesValues.conso ) - ( slidesValues.km * getCurrentModel().WLTP_consumption / 100 * slidesValues.km)  }
-      {txt === "Sur 1 an" && (slidesValues.km * slidesValues.conso / 100  * slidesValues.conso ) - ( slidesValues.km * getCurrentModel().WLTP_consumption / 100 * slidesValues.km) < 0 ? 0 : position === 0 && (slidesValues.km * slidesValues.conso / 100  * slidesValues.conso ) - ( slidesValues.km * getCurrentModel().WLTP_consumption / 100 * slidesValues.km)  }
-      {txt === "Sur 4 ans" && (slidesValues.km * slidesValues.conso / 100  * slidesValues.conso ) - ( slidesValues.km * getCurrentModel().WLTP_consumption / 100 * slidesValues.km) < 0 ? 0 : position === 0 && (slidesValues.km * slidesValues.conso / 100  * slidesValues.conso ) - ( slidesValues.km * getCurrentModel().WLTP_consumption / 100 * slidesValues.km)  }
+      {txt === "Sur 1 mois" && Math.floor(((slidesValues.km * slidesValues.conso / 100  * slidesValues.carbu) - ( slidesValues.km * getCurrentModel().WLTP_consumption / 100 * slidesValues.elec) < 0 ? 0 : (slidesValues.km * slidesValues.conso / 100  * slidesValues.carbu) - ( slidesValues.km * getCurrentModel().WLTP_consumption / 100 * slidesValues.elec)) / 12) }
+      {txt === "Sur 1 an" && Math.floor(slidesValues.km * slidesValues.conso / 100  * slidesValues.carbu) - ( slidesValues.km * getCurrentModel().WLTP_consumption / 100 * slidesValues.elec) < 0 ? 0 : (slidesValues.km * slidesValues.conso / 100  * slidesValues.carbu) - ( slidesValues.km * getCurrentModel().WLTP_consumption / 100 * slidesValues.elec) } 
+      {txt === "Sur 4 ans" && Math.floor(((slidesValues.km * slidesValues.conso / 100  * slidesValues.carbu) - ( slidesValues.km * getCurrentModel().WLTP_consumption / 100 * slidesValues.elec) < 0 ? 0 : (slidesValues.km * slidesValues.conso / 100  * slidesValues.carbu) - ( slidesValues.km * getCurrentModel().WLTP_consumption / 100 * slidesValues.elec)) * 4) }      
         <div className="result-symbol">{symbol}</div>
       </span>
       <p className="result-info">
           {txt}
         </p>
+
+
+
     </div>
   );
 }
